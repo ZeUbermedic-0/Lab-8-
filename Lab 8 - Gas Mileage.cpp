e0e58de30f7bcc38a380435b0fbd185f11e69e2a
ZeUbermedic-0 <bahsidy0701@gmail.com>
2025-10-31 14:38:57 -0500

    Added column dashes.

--- Lab 8 - Gas Mileage.cpp
+++ Lab 8 - Gas Mileage.cpp
@@ -10,8 +10,8 @@ void CalcAverage();
 
 int main()
 {
-   CalcAverage();  //Function Call 
-   return 0;
+    CalcAverage();  //Function Call 
+    return 0;
 }
 
 //(2)Function Definition (Intialization)
@@ -25,8 +25,8 @@ void CalcAverage()
 
     for (int i = 0; i < weeks; ++i)                                    //Loop to get starting date for each week
     {
-       cout << "Enter the starting date for week " << i + 1 << ": ";
-	   getline(cin, startingdate[i]);                                  //Get starting date input
+        cout << "Enter the starting date for week " << i + 1 << ": ";
+        getline(cin, startingdate[i]);                    //Get starting date input
     }
 
     for (int i = 0; i < weeks; ++i)                                     //Loop for each week
@@ -38,64 +38,66 @@ void CalcAverage()
         // corresponding starting date aka "string"
         cout << "Week " << i + 1 << " (" << startingdate[i] << "):\n";
 
-		//(5)Prompt user 
+        //(5)Prompt user 
         cout << "Enter number of miles driven: ";
         cin >> miles;
         cout << "Enter number of gallons used: ";
         cin >> gallons;
 
         //(6) Update running miles & gallons totals
-        aMiles += miles;           
-        aGallons += gallons;       
+        aMiles += miles;
+        aGallons += gallons;
 
         //(7)Store weekly miles & weekly gallons in array  
-        totalMiles[i] = miles;            
-        totalGallons[i] = gallons;       
-         
-		//() Display weekly gas mileage
-        cout << "Gas mileage for week " 
-             << i + 1 << ": " << fixed << setprecision(2) 
-             << miles / gallons << " miles per gallon\n";
+        totalMiles[i] = miles;
+        totalGallons[i] = gallons;
+
+        //() Display weekly gas mileage
+        cout << "Gas mileage for week "
+            << i + 1 << ": " << fixed << setprecision(2)
+            << miles / gallons << " miles per gallon\n";
         cin.ignore();
 
         //(8) Display running totals
         cout << "Total miles driven: " << aMiles << endl;
         cout << "Total gallons used: " << aGallons << endl;
-        cout << "Overall gas mileage: " << fixed << setprecision(2) 
-             << aMiles / aGallons << " miles per gallon\n";
+        cout << "Overall gas mileage: " << fixed << setprecision(2)
+            << aMiles / aGallons << " miles per gallon\n";
     }
-    
+
     //(9)Format and display table header
     cout << '\n';
-    cout << setw(12) << "Week of"
-         << setw(10) << "Gallons"
-         << setw(10) << "Miles"
-         << setw(10) << "Average" << '\n';    //New column
+    cout << setw(18) << "Week of";
+
+    cout << setw(16) << "Gallons";
+
+    cout << setw(16) << "Miles";
+
+    cout << setw(16) << "Average" << '\n';    //New column
     cout << '\n';
 
     //(10)Format and display dash lines
-    //cout << "\n";
-    //cout << setw(12) << "- - - - - - "
-    //     << '\n';
+    cout << "\n";
+    cout << setw(18) << "       - - - - - - \t- - - - - -\t- - - - - -\t- - - - - -"
+        << '\n';
 
     //(11) Display detailed weekly breakdown
     for (int i = 0; i < weeks; ++i)
     {
         //Display weekly data for each week accumulated so far
         //it iterates through all recorded weeks to show the running history
-        cout << setw(12) << startingdate[i]
-             << setw(10) << totalGallons[i]
-             << setw(10) << totalMiles[i]
-             << setw(10) << totalMiles[i] / totalGallons[i] << endl;             
+        cout << setw(18) << startingdate[i]
+            << setw(16) << totalGallons[i]
+            << setw(16) << totalMiles[i]
+            << setw(16) << totalMiles[i] / totalGallons[i] << endl;
     }
     //(12) Display totals row
-    cout << setw(12) << "Totals"
-         << setw(10) << aGallons
-         << setw(10) << aMiles
-         << setw(10) << aMiles / aGallons << endl;
+    cout << setw(18) << "Totals"
+        << setw(16) << aGallons
+        << setw(16) << aMiles
+        << setw(16) << aMiles / aGallons << endl;
 
 
-}   
-        
-         
-       
+}
+
+
